d0562ab0a8a56018b70ef3d0b079ea39cec17c5a
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-05 09:35:58 -0400

    Stub out CLI data and add notes file

--- lib/rt_fresh_tv/cli.rb
+++ lib/rt_fresh_tv/cli.rb
@@ -2,7 +2,16 @@
 class RtFreshTv::CLI
 
 	def call
-		puts "Hello World"
+		puts "Welcome to Rotten Tomatoes' Fresh TV!"
+		list_shows
 	end
+
+	def list_shows
+		puts "1. Vida: Season 1. 100%"
+		puts "2. GLOW: Season 2. 98%"
+		puts "3. The Handmaid's Tale: Season 2. 96%"
+		puts "4. Pose: Season 1. 96%"
+	end
+
 
 end
