9e49dda8cd8b97b64613a5e2ce32063e3db3ba8b
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-05 16:13:27 -0400

    Refine menu method in CLI to display more information

--- lib/rt_fresh_tv/cli.rb
+++ lib/rt_fresh_tv/cli.rb
@@ -12,15 +12,11 @@ class RtFreshTv::CLI
 		puts "****************************************"
 		puts "* Welcome to Rotten Tomatoes' Fresh TV *"
 		puts "****************************************"
-		puts "Below, you will find current television shows that have been 'Certified Fresh' on Rotten Tomatoes."
+		puts "Below, you will find current television shows that have been 'Certified Fresh' on Rotten Tomatoes, along with their Tomato-Meter scores."
 	end
 
 	
 	def list_shows
-#		puts "1. Vida: Season 1. 100% Fresh"
-#		puts "2. GLOW: Season 2. 98% Fresh"
-#		puts "3. The Handmaid's Tale: Season 2. 96% Fresh"
-#		puts "4. Pose: Season 1. 96% Fresh"
 		@shows = RtFreshTv::Show.all
 		@shows.each.with_index(1) do |show, i|
 			puts "#{i}- #{show.title}. #{show.rating} Fresh"
@@ -33,9 +29,9 @@ class RtFreshTv::CLI
 			puts "Please type the number of the show you would like more information about. You can also type 'list' for the list of shows or 'exit'."
 			input = gets.strip.downcase
 			if input.to_i > 0 && input.to_i <= @shows.size
-				puts "Synopsis: #{@shows[input.to_i - 1].synopsis}"
-				puts "Critic Consensus: #{@shows[input.to_i - 1].critic_consensus}"
-				puts "For more reviews and information, visit #{@shows[input.to_i - 1].url}"
+				puts "Synopsis: #{@shows[input.to_i - 1].synopsis} \n\n"
+				puts "Critic Consensus: #{@shows[input.to_i - 1].critic_consensus} \n\n"
+				puts "For more reviews and information, visit #{@shows[input.to_i - 1].url} \n\n"
 			elsif input == "list"
 				list_shows
 			elsif input == "exit"
