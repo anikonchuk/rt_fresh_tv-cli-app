5f1a3de9fffd84bbf6a7b6428e723c031eb815c8
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-05 14:01:25 -0400

    Build simple show class

--- lib/rt_fresh_tv/show.rb
+++ lib/rt_fresh_tv/show.rb
@@ -1,4 +1,15 @@
 class RtFreshTv::Show
 
+	attr_accessor :name, :URL, :rating, :synopsis, :critic_consensus
 
+	@@all = []
+
+	def initialize
+		@@all << self
+	end
+
+	def self.all 
+		@@all
+	end
+	
 end
