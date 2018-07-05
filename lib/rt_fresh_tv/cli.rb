b4af4d9c063412a0172cdc0e0520e09cfe545e67
Andrew Nikonchuk <anikonch@gmail.com>
2018-07-05 10:49:55 -0400

    Modify gemspec file to run console

--- lib/rt_fresh_tv/cli.rb
+++ lib/rt_fresh_tv/cli.rb
@@ -10,13 +10,13 @@ class RtFreshTv::CLI
 		puts "Welcome to Rotten Tomatoes' Fresh TV"
 		puts "Below, you will find current television shows that have been 'Certified Fresh' on Rotten Tomatoes."
 	end
-	
+
 	
 	def list_shows
-		puts "1. Vida: Season 1. 100%"
-		puts "2. GLOW: Season 2. 98%"
-		puts "3. The Handmaid's Tale: Season 2. 96%"
-		puts "4. Pose: Season 1. 96%"
+		puts "1. Vida: Season 1. 100% Fresh"
+		puts "2. GLOW: Season 2. 98% Fresh"
+		puts "3. The Handmaid's Tale: Season 2. 96% Fresh"
+		puts "4. Pose: Season 1. 96% Fresh"
 	end
 
 
